476ba427a27579400a7e1d6baca3ae84283a6586
Paul Hayes <fofr@users.noreply.github.com>
2018-04-13 14:01:37 +0100

    Deduplicate funding templates
    Avoid having to update two templates when alerting design

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,11 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+router.get('/results/filters/funding', function(req, res) {
+  backLink = { text: 'Back to results', href: '/results'}
+  res.render('start/funding', { 'backLink': backLink });
+});
+
 // add your routes here
 
 router.get('/v04/q3', function(req, res) {
