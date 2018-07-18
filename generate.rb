e0167cb15ff1f458753db3381d2c372d2ed89d93
Paul Hayes <fofr@users.noreply.github.com>
2018-07-18 11:51:02 +0100

    Add courses

--- generate.rb
+++ generate.rb
@@ -4,12 +4,57 @@
 require 'json'
 file = File.read('courses-clean.json')
 data = JSON.parse(file)
+sample = data.reject {|c| c['campuses'].empty? }.sample(400)
+
+def to_slug(string)
+  string.downcase.gsub(/[^a-zA-Z0-9]/, '-').gsub(/--*/, '-').gsub(/-$/,'')
+end
 
 prototype_data = {
   "study-type": ["Full time (12 months)", "Part time (18-24 months)"],
-  "qualification": ["Postgraduate certificate in education with qualified teacher status", "Qualified teacher status"],
-  "results": []
+  "qualification": ["Postgraduate certificate in education with qualified teacher status", "Qualified teacher status"]
 }
 
+prototype_data['subjects'] = data.map { |c| c['subjects'].map {|s| s.downcase.capitalize } }.flatten.uniq.sort
+
+# Clean up subjects
+prototype_data['subjects'].reject! { |s| s.include?('abridged') || s.include?('Secondary') }
+
+prototype_data['courses'] = sample.map do |c|
+  options = []
+
+  if !c['qualifications'] || c['qualifications'].length == 0
+    qual = "Unknown"
+  else
+    qual = (c['qualifications'].include?('Postgraduate') || c['qualifications'].include?('Professional')) ? 'PGCE with QTS' : 'QTS'
+  end
+  partTime = c['campuses'].map {|g| g['partTime'] }.uniq.reject {|r| r == "n/a"}.count > 0
+  fullTime = c['campuses'].map {|g| g['fullTime'] }.uniq.reject {|r| r == "n/a"}.count > 0
+  salaried = c['route'] == "School Direct training programme (salaried)" ? ' with salary' : ''
+
+  if partTime
+    options << "#{qual}, part time#{salaried}"
+  end
+
+  if fullTime
+    options << "#{qual}, full time#{salaried}"
+  end
+
+  course = {
+    locationType: c['route'] == 'Higher Education programme' ? 'University' : 'School',
+    accrediting: c['accrediting'],
+    provider: c['provider'].gsub("'", "’"),
+    subjects: c['subjects'].map {|s| s.downcase.capitalize },
+    name: c['name'],
+    slug: "#{c['providerCode']}/#{c['programmeCode']}",
+    providerCode: c['providerCode'],
+    programmeCode: c['programmeCode'],
+    schools: c['campuses'].map { |a| { name: a['name'], address: a['address'], code: a['code'] } },
+    addresses: c['campuses'].map { |a| a['address'] },
+    options: options
+  }
+
+  course
+end
 
 File.open('lib/prototype_data.json', 'w') { |file| file.write(JSON.pretty_generate(prototype_data) + "\n") }
