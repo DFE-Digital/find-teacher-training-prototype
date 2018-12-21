387e8bd68d4d29617370ce023fe3c34609728010
Paul Hayes <fofr@users.noreply.github.com>
2018-12-21 10:15:09 +0000

    Fix name of service in config

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Find postgraduate teacher training in business studies',
+  serviceName: 'Find postgraduate teacher training',
 
   // Default port that prototype runs on
   port: '3000',
