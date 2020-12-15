851ee74c7cd7e0dce440945884d80a704882ed84
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-12-15 17:37:43 +0000

    Filter by qualification

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,4 +1,17 @@
 module.exports = {
+  qualificationOptions: [{
+    text: 'QTS only',
+    hint: 'QTS (qualified teacher status) allows you to teach in state schools in England and may allow you to teach in other parts of the UK.\n\nIt may also allow you to teach in the [EU and EEA](https://www.gov.uk/eu-eea), though this could change after 2020. If you’re planning to teach overseas, you should always check what’s needed in the country you’d like to teach in.',
+    value: 'qts'
+  }, {
+    text: 'PGCE (or PGDE) with QTS',
+    hint: 'A PGCE (postgraduate certificate in education) is an academic qualification in education. PGCE with QTS allows you to teach in state schools in England and may allow you to teach in other parts of the UK.\n\nIt may also allow you to teach in the [EU and EEA](https://www.gov.uk/eu-eea), though this could change after 2020. If you’re planning to teach overseas, you should always check what’s needed in the country you’d like to teach in.\n\nMany PGCE courses include credits towards a Master’s degree. Some providers offer a PGDE (postgraduate diploma in education) with QTS, which is equivalent to a PGCE. In some cases these offer more Master’s credits than PGCE courses.',
+    value: 'pgce_with_qts,pgde_with_qts'
+  }, {
+    text: 'Further education (PGCE or PGDE without QTS)',
+    hint: 'To teach further education you don’t need QTS. Instead you can study for a PGCE or PGDE without QTS.',
+    value: 'pgce,pgde'
+  }],
   salaryOptions: [{
     text: 'Courses with and without salary',
     value: 'salary,apprenticeship,fee'
@@ -22,13 +35,10 @@ module.exports = {
     text: 'Courses with and without vacancies',
     value: 'false'
   }],
+  selectedQualificationOption: ['qts'],
   selectedSalaryOption: 'salary,apprenticeship,fee',
   selectedStudyTypes: ['full_time', 'part_time'],
   selectedVacancyOption: 'false',
-  qualification: [
-    'PGCE with QTS (Postgraduate certificate in education with qualified teacher status)',
-    'QTS (Qualified teacher status)'
-  ],
   latLong: {
     lat: 52.6033,
     lng: -1.4183
