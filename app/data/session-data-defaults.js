0dc6293023b6cff622cc73481525b1b862f08e8a
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-12-15 16:53:18 +0000

    Filter by vacancy

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -15,8 +15,16 @@ module.exports = {
     hint: 'You’ll usually spend 3 days a week on a part-time course',
     value: 'part_time'
   }],
+  vacancyOptions: [{
+    text: 'Only courses with vacancies',
+    value: 'true'
+  }, {
+    text: 'Courses with and without vacancies',
+    value: 'false'
+  }],
   selectedSalaryOption: 'salary,apprenticeship,fee',
   selectedStudyTypes: ['full_time', 'part_time'],
+  selectedVacancyOption: 'false',
   qualification: [
     'PGCE with QTS (Postgraduate certificate in education with qualified teacher status)',
     'QTS (Qualified teacher status)'
