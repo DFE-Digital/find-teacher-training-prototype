871d23d773a0c763e85673cb1f2bb2181389a3c9
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-12-15 20:36:30 +0000

    Remove previous subject filter

--- app/routes.js
+++ app/routes.js
@@ -12,15 +12,13 @@ router.post('/start/location', function (req, res) {
 })
 
 router.get('/results/filters/location', function (req, res) {
-  const isMap = req.query.map
-  const backLink = { text: 'Back to search results', href: isMap ? '/results?map=yes' : '/results' }
+  const backLink = { text: 'Back to search results', href: '/results' }
   res.render('start/location', { backLink: backLink, filtering: true })
 })
 
 router.post('/results/filters/location', function (req, res) {
-  const isMap = req.body.map
-  const backLink = { text: 'Back to search results', href: isMap ? '/results?map=yes' : '/results' }
-  handleLocationSearch(req.body['postcode-town-or-city'], req, res, isMap ? '/results?map=yes' : '/results', { backLink: backLink, filtering: true })
+  const backLink = { text: 'Back to search results', href: '/results' }
+  handleLocationSearch(req.body['postcode-town-or-city'], req, res, '/results', { backLink: backLink, filtering: true })
 })
 
 function handleLocationSearch (location, req, res, successRedirect, options = {}) {
@@ -42,76 +40,6 @@ function handleLocationSearch (location, req, res, successRedirect, options = {}
   } else {
     res.redirect(successRedirect)
   }
-}
-
-router.get('/results/filters/subjects', function (req, res) {
-  const backLink = { text: 'Back to search results', href: '/results' }
-  res.render('start/subjects', { subjectGroups: subjectGroups(req), filtering: true, backLink: backLink })
-})
-
-router.get('/start/subjects', function (req, res) {
-  res.render('start/subjects', { subjectGroups: subjectGroups(req) })
-})
-
-function subjectGroups (req) {
-  const subjectGroups = []
-
-  for (const subjectGroup of Object.keys(req.session.data.groupedSubjects)) {
-    const subjects = []
-    const scholarshipAndBursarySubjects = [
-      'Chemistry',
-      'Geography',
-      'Physics',
-      'Computing',
-      'French',
-      'Spanish',
-      'German'
-    ]
-
-    const lowerBursarySubjects = [
-      'Biology',
-      'Classics'
-    ]
-
-    const humanitiesBursarySubjects = [
-      'History',
-      'Music',
-      'Religious education'
-    ]
-
-    req.session.data.groupedSubjects[subjectGroup].forEach(function (subject) {
-      let hint
-      if (scholarshipAndBursarySubjects.includes(subject)) {
-        hint = 'Scholarships and bursaries up to £28,000 available.'
-      } else if (subject === 'English') {
-        hint = 'Bursaries of £15,000 available.'
-      } else if (lowerBursarySubjects.includes(subject)) {
-        hint = 'Bursaries of £26,000 available.'
-      } else if (humanitiesBursarySubjects.includes(subject)) {
-        hint = 'Bursaries of up to £9,000 available.'
-      } else if (subject.includes('Design and technology')) {
-        hint = 'Bursaries of up to £12,000 available.'
-      } else if (subject.includes('Mathematics')) {
-        hint = 'Scholarships and bursaries up to £22,000 available with further early career payments up to £10,000.'
-      }
-
-      // TODO: Primary maths too (6,000)
-      subjects.push({
-        value: subject,
-        text: subject,
-        label: {
-          classes: 'govuk-label--s'
-        },
-        hint: {
-          text: hint
-        }
-      })
-    })
-
-    subjectGroups.push({ group: subjectGroup, subjects: subjects })
-  }
-
-  return subjectGroups
 }
 
 require('./routes/results')(router)
