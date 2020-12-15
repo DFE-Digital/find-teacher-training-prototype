79fc8957f952bcd458d4d36aaed2fbb3ce84d418
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-12-15 16:53:18 +0000

    Filter by vacancy

--- app/routes/filters.js
+++ app/routes/filters.js
@@ -1,4 +1,24 @@
 module.exports = router => {
+  // Salary
+  router.get('/results/filters/salary', function (req, res) {
+    const { salaryOptions, selectedSalaryOption } = req.session.data
+
+    const backLink = {
+      text: 'Back to search results',
+      href: '/results'
+    }
+
+    const items = salaryOptions.map(option => {
+      return {
+        value: option.value,
+        text: option.text,
+        checked: selectedSalaryOption === option.value
+      }
+    })
+
+    res.render('filters/salary', { backLink, items })
+  })
+
   // Study type
   router.get('/results/filters/study-type', function (req, res) {
     const { studyTypes, selectedStudyTypes } = req.session.data
@@ -21,22 +41,23 @@ module.exports = router => {
     res.render('filters/study-type', { backLink, items })
   })
 
-  router.get('/results/filters/salary', function (req, res) {
-    const { salaryOptions, selectedSalaryOption } = req.session.data
+  // Vacancies
+  router.get('/results/filters/vacancy', function (req, res) {
+    const { vacancyOptions, selectedVacancyOption } = req.session.data
 
     const backLink = {
       text: 'Back to search results',
       href: '/results'
     }
 
-    const items = salaryOptions.map(option => {
+    const items = vacancyOptions.map(option => {
       return {
         value: option.value,
         text: option.text,
-        checked: selectedSalaryOption === option.value
+        checked: selectedVacancyOption === option.value
       }
     })
 
-    res.render('filters/salary', { backLink, items })
+    res.render('filters/vacancy', { backLink, items })
   })
 }
