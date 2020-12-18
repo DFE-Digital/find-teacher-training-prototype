b2cb52f1acd58c18dfc90cba2115ce2b376880b3
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-12-18 00:03:15 +0000

    Refactor filter sidebar (behind feature flag)

--- app/routes/filters.js
+++ app/routes/filters.js
@@ -1,4 +1,3 @@
-const filters = require('../filters')()
 const utils = require('../utils')()
 
 const backLink = {
@@ -14,73 +13,42 @@ module.exports = router => {
 
   // Qualification
   router.get('/results/filters/qualification', (req, res) => {
-    const { qualificationOptions, qualification } = req.session.data
-
-    const items = qualificationOptions.map(option => {
-      return {
-        value: option.value,
-        text: option.text,
-        label: { classes: 'govuk-label--s' },
-        hint: { text: filters.markdown(option.hint) },
-        checked: qualification.includes(option.value)
-      }
+    res.render('filters/qualification', {
+      backLink,
+      items: utils.qualificationItems(req.session.data.qualification)
     })
-
-    res.render('filters/qualification', { backLink, items })
   })
 
   // Salary
   router.get('/results/filters/salary', function (req, res) {
-    const { salaryOptions, salary } = req.session.data
-
-    const items = salaryOptions.map(option => {
-      return {
-        value: option.value,
-        text: option.text,
-        checked: salary === option.value
-      }
+    res.render('filters/salary', {
+      backLink,
+      items: utils.salaryItems(req.session.data.salary)
     })
-
-    res.render('filters/salary', { backLink, items })
-  })
-
-  // Subject/SEND
-  router.get('/results/filters/subject', async (req, res) => {
-    const { levels, send, subjectOptions } = req.session.data
-    const groups = utils.getSubjectGroups(levels, send, subjectOptions)
-
-    res.render('filters/subject', { backLink, groups, send })
   })
 
   // Study type
   router.get('/results/filters/study-type', function (req, res) {
-    const { studyTypeOptions, studyType } = req.session.data
-
-    const items = studyTypeOptions.map(option => {
-      return {
-        value: option.value,
-        text: option.text,
-        label: { classes: 'govuk-label--s' },
-        hint: { text: option.hint },
-        checked: studyType.includes(option.value)
-      }
+    res.render('filters/study-type', {
+      backLink,
+      items: utils.studyTypeItems(req.session.data.studyType)
     })
+  })
 
-    res.render('filters/study-type', { backLink, items })
+  // Subject/SEND
+  router.get('/results/filters/subject', async (req, res) => {
+    const { send, subjects } = req.session.data
+    res.render('filters/subject', {
+      backLink,
+      items: utils.subjectGroupItems(send, subjects)
+    })
   })
 
   // Vacancies
   router.get('/results/filters/vacancy', function (req, res) {
-    const { vacancyOptions, vacancy } = req.session.data
-
-    const items = vacancyOptions.map(option => {
-      return {
-        value: option.value,
-        text: option.text,
-        checked: vacancy === option.value
-      }
+    res.render('filters/vacancy', {
+      backLink,
+      items: utils.vacancyItems(req.session.data.vacancy)
     })
-
-    res.render('filters/vacancy', { backLink, items })
   })
 }
