1b37ff61c150134e11f4d5ada9ba5e15ebe0cd31
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-01-15 19:04:10 +0000

    Simplify index

--- app/routes/start.js
+++ app/routes/start.js
@@ -1,49 +1,6 @@
 const utils = require('../utils')()
 
 module.exports = router => {
-  router.get('/', (req, res) => {
-    const {
-      defaults,
-      salaryOptions,
-      studyTypeOptions
-    } = req.session.data
-
-    const levelItems = [{
-      value: 'primary',
-      text: 'Primary'
-    }, {
-      value: 'secondary',
-      text: 'Secondary'
-    }, {
-      value: 'further_education',
-      text: 'Further education'
-    }]
-
-    const salaryItems = salaryOptions.map(option => {
-      const salary = req.session.data.salary || defaults.salary
-      return {
-        value: option.value,
-        text: option.text,
-        checked: salary === option.value
-      }
-    })
-
-    const studyTypeItems = studyTypeOptions.map(option => {
-      const studyType = req.session.data.studyType || defaults.studyType
-      return {
-        value: option.value,
-        text: option.text,
-        checked: studyType.includes(option.value)
-      }
-    })
-
-    res.render('index', {
-      levelItems,
-      salaryItems,
-      studyTypeItems
-    })
-  })
-
   router.all('/search', async (req, res) => {
     const qurey = req.session.data.q || req.query.q
     const queryType = await utils.processQuery(qurey, req.session.data)
