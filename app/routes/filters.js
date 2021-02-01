9dd9cd798feb35b023a0b5b775e657b76b62d484
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-02-01 14:19:53 +0000

    Remove old filter sidebar

--- app/routes/filters.js
+++ app/routes/filters.js
@@ -6,11 +6,6 @@ const backLink = {
 }
 
 module.exports = router => {
-  // Location
-  router.get('/results/filters/location', (req, res) => {
-    res.render('filters/location', { backLink })
-  })
-
   // London
   router.get('/results/filters/london', (req, res) => {
     res.render('filters/london', {
@@ -25,49 +20,6 @@ module.exports = router => {
         west: utils.londonBoroughItems(req.session.data.londonBorough, { regionFilter: 'west' })
       },
       next: '/results'
-    })
-  })
-
-  // Qualification
-  router.get('/results/filters/qualification', (req, res) => {
-    res.render('filters/qualification', {
-      backLink,
-      items: utils.qualificationItems(req.session.data.qualification)
-    })
-  })
-
-  // Salary
-  router.get('/results/filters/salary', function (req, res) {
-    res.render('filters/salary', {
-      backLink,
-      items: utils.salaryItems(req.session.data.salary)
-    })
-  })
-
-  // Study type
-  router.get('/results/filters/study-type', function (req, res) {
-    res.render('filters/study-type', {
-      backLink,
-      items: utils.studyTypeItems(req.session.data.studyType)
-    })
-  })
-
-  // Subject/SEND
-  router.get('/results/filters/subject', async (req, res) => {
-    res.render('filters/subject', {
-      backLink,
-      subjectItems: utils.subjectItems(req.session.data.subjects, {
-        showHintText: true
-      }),
-      sendItems: utils.sendItems(req.session.data.send)
-    })
-  })
-
-  // Vacancies
-  router.get('/results/filters/vacancy', function (req, res) {
-    res.render('filters/vacancy', {
-      backLink,
-      items: utils.vacancyItems(req.session.data.vacancy)
     })
   })
 }
