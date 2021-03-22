4ce774be2c1fe7b0eca45d730b9768ae395b394f
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-03-22 18:09:46 +0000

    Course page updates

--- app/routes/course.js
+++ app/routes/course.js
@@ -9,12 +9,55 @@ module.exports = router => {
       const courseSingleResponse = await teacherTrainingService.getCourse(providerCode, courseCode)
       const course = utils.decorateCourse(courseSingleResponse.data.attributes)
 
+      const LocationListResponse = await teacherTrainingService.getCourseLocations(providerCode, courseCode)
+      const statuses = LocationListResponse.included.filter(item => item.type === 'location_statuses')
+      const locations = LocationListResponse.data.map(location => {
+        const { attributes } = location
+
+        // Vacancy status
+        const statusId = location.relationships.location_status.data.id
+        const status = statuses.find(status => status.id === statusId)
+        attributes.has_vacancies = status.attributes.has_vacancies
+
+        // Address
+        const streetAddress1 = attributes.street_address_1 ? attributes.street_address_1 + ', ' : ''
+        const streetAddress2 = attributes.street_address_2 ? attributes.street_address_2 + ', ' : ''
+        const city = attributes.city ? attributes.city + ', ' : ''
+        const county = attributes.county ? attributes.county + ', ' : ''
+        const postcode = attributes.postcode
+
+        attributes.name = attributes.name.replace(/'/g, '’')
+        attributes.address = `${streetAddress1}${streetAddress2}${city}${county}${postcode}`
+
+        return attributes
+      })
+
       // Get travel areas that school placements lie within
       // Fake it by adding current travel area being to list of placements
-      const fakedPlacementArea = req.session.data.area ? req.session.data.area.name : false
-      const placementAreas = await utils.getPlacementAreas(providerCode, courseCode, fakedPlacementArea)
+      //
+      // const fakedPlacementArea = req.session.data.area ? req.session.data.area.name : false
+      // const placementAreas = await utils.getPlacementAreas(providerCode, courseCode, fakedPlacementArea)
 
-      res.render('course', { course, placementAreas })
+      // Mock placement schools
+      // Assume placement schools are locations with a campus code that isn’t '-'
+      const schools = locations.filter(location => location.code !== '-')
+
+      // Mock school placement policy
+      // Assume provider has chosen to show placement schools if a course has more than 1 school location
+      course.placementPolicy = schools.length > 1 ? 'hosted' : 'placed'
+
+      // Mock centre-based training location
+      // Assume centre-based training location is location with campus code '-'
+      const mainTrainingLocation = locations.find(location => location.code === '-')
+      const trainingLocationOptions = ['remote', 'school', mainTrainingLocation]
+
+      course.trainingLocation = trainingLocationOptions[Math.floor(Math.random() * trainingLocationOptions.length)]
+
+      res.render('course', {
+        course,
+        schools
+        // placementAreas
+      })
     } catch (error) {
       res.render('error', {
         title: error.name,
