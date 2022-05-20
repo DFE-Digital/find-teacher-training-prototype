3074881aef597f762279a19db61b25b7788e0ff0
Simon Whatley <simonwhatley@gmail.com>
2022-05-20 12:13:53 +0100

    Update gulp files

--- gulp/nodemon.js
+++ gulp/nodemon.js
@@ -30,7 +30,7 @@ const onQuit = () => {
 
 gulp.task('server', function () {
   nodemon({
-    watch: ['.env', '**/*.js', '**/*.json'],
+    watch: ['.env', '**/*.js'], //, '**/*.json'
     script: 'listen-on-port.js',
     ignore: [
       config.paths.public + '*',
