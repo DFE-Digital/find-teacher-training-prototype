39a8d028162c98e02177ba93ea1a408b5988c89e
Simon Whatley <simonwhatley@gmail.com>
2022-06-17 14:46:27 +0100

    Update course decorator

--- app/utils.js
+++ app/utils.js
@@ -21,88 +21,6 @@ module.exports = () => {
   const utils = {}
 
   utils.decorateCourse = course => {
-    // Placeholder until API is updated
-    course.requirements = {
-      degree: {
-      },
-      gcses: {
-        english: {},
-        maths: {},
-        science: {}
-      }
-    }
-
-    // Adding degree subject requirement unless it’s primary
-    if (course.name !== 'Primary') {
-      course.requirements.degree.subject = '50% of your degree modules should be in the subject.'
-    }
-
-    // Randomising whether pending GCSEs are accepted or not.
-    switch (getRandomInt(2)) {
-      case 0:
-        course.requirements.gcses.pendingGcsesAccepted = true
-        break
-      default:
-        course.requirements.gcses.pendingGcsesAccepted = false
-        break
-    }
-
-    // Randomising whether equivalency tests accepted or not.
-    switch (getRandomInt(2)) {
-      case 0:
-        course.requirements.gcses.equivalencyTestsAccepted = true
-        break
-      default:
-        course.requirements.gcses.equivalencyTestsAccepted = false
-        break
-    }
-
-    if (course.requirements.gcses.equivalencyTestsAccepted) {
-      // Randomising which subjects tests accepted for
-      switch (getRandomInt(5)) {
-        case 0:
-          course.requirements.gcses.equivalencyTestSubjects = ['maths']
-          break
-        case 1:
-          course.requirements.gcses.equivalencyTestSubjects = ['English']
-          break
-        case 2:
-          course.requirements.gcses.equivalencyTestSubjects = ['English', 'maths']
-          break
-        default:
-          if (course.name === 'Primary') {
-            course.requirements.gcses.equivalencyTestSubjects = ['English', 'maths', 'science']
-          } else {
-            course.requirements.gcses.equivalencyTestSubjects = ['English', 'maths']
-          }
-          break
-      }
-    }
-
-    switch (getRandomInt(5)) {
-      case 0:
-        course.requirements.gcses.maths.flexibility = 'must'
-        course.requirements.gcses.english.flexibility = 'must'
-        if (course.name === 'Primary') {
-          course.requirements.gcses.science.flexibility = 'must'
-        }
-        break
-      case 1:
-        course.requirements.gcses.maths.flexibility = 'pending'
-        course.requirements.gcses.english.flexibility = 'pending'
-        if (course.name === 'Primary') {
-          course.requirements.gcses.science.flexibility = 'pending'
-        }
-        break
-      default:
-        course.requirements.gcses.maths.flexibility = 'equivalency-test-offered'
-        course.requirements.gcses.english.flexibility = 'equivalency-test-offered'
-        if (course.name === 'Primary') {
-          course.requirements.gcses.science.flexibility = 'equivalency-test-offered'
-        }
-        break
-    }
-
     // SKE
     const subjectCodesWithSke = data.subjectOptions
       .filter(subject => subject.hasSke === true)
@@ -111,13 +29,36 @@ module.exports = () => {
     course.has_ske = subjectCodesWithSke.some(code => course.subject_codes.includes(code))
 
     // Funding
+    if (course.has_bursary === undefined) {
+      if (course.bursary_amount === 'null') {
+        course.has_bursary = false
+      } else {
+        course.has_bursary = true
+      }
+    }
+
+    if (course.has_scholarship === undefined) {
+      if (course.scholarship_amount === 'null') {
+        course.has_scholarship = false
+      } else {
+        course.has_scholarship = true
+      }
+    }
+
     course.has_fees = course.funding_type === 'fee'
     course.salaried = course.funding_type === 'salary' || course.funding_type === 'apprenticeship'
-    course.funding_option = course.salaried ? 'Salary' : 'Student finance if you’re eligible'
-    // course.has_bursary = course.name.includes('Chemistry') // Stub. See https://github.com/DFE-Digital/find-teacher-training/blob/94de46eea7ddeec2daca2e4944b9bf4582d25304/app/decorators/course_decorator.rb#L47
-    // course.has_scholarship = true // Stub. See https://github.com/DFE-Digital/find-teacher-training/blob/94de46eea7ddeec2daca2e4944b9bf4582d25304/app/decorators/course_decorator.rb#L59
     course.bursary_only = course.has_bursary && !course.has_scholarship
     course.has_scholarship_and_bursary = course.has_bursary && course.has_scholarship
+
+    if (course.salaried) {
+      course.funding_option = 'Salary'
+    } else if (course.has_scholarship_and_bursary) {
+      course.funding_option = 'Scholarships or bursaries, as well as student finance, are available if you’re eligible'
+    } else if (course.has_bursary) {
+      course.funding_option = 'Bursaries and student finance are available if you’re eligible'
+    } else {
+      course.funding_option = 'Student finance if you’re eligible'
+    }
 
     // Year range
     course.year_range = `${data.cycle} to ${Number(data.cycle) + 1}`
