12d4f9d1d63b508789c89f59a0b06495c90df60f
Simon Whatley <simonwhatley@gmail.com>
2022-07-11 11:11:38 +0100

    Update autocomplete scripts

--- app/assets/javascripts/init-autocomplete.js
+++ app/assets/javascripts/init-autocomplete.js
@@ -1,11 +1,46 @@
-const initAutocomplete = ({element, input, path, selectNameAndCode}) => {
+import accessibleAutocomplete from "accessible-autocomplete";
+import debounce from 'lodash.debounce';
+
+export const getPath = (endpoint, query) => {
+  return `${endpoint}?query=${query}`;
+}
+
+export const request = endpoint => {
+  let xhr = null; // Hoist this call so that we can abort previous requests.
+
+  return (query, callback) => {
+    if (xhr && xhr.readyState !== XMLHttpRequest.DONE) {
+      xhr.abort();
+    }
+    const path = getPath(endpoint, query);
+
+    xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", evt => {
+      let results = [];
+      try {
+        results = JSON.parse(xhr.responseText);
+      } catch (err) {
+        console.error(
+          `Failed to parse results from endpoint ${path}, error is:`,
+          err
+        );
+      }
+      callback(results);
+    });
+    xhr.open("GET", path);
+    xhr.send();
+  };
+};
+
+const initAutocomplete = ({ element, input, path, selectNameAndCode }) => {
   const $input = document.getElementById(input);
   const $el = document.getElementById(element);
+  if (!$el) return;
+
   const inputValueTemplate = result => (typeof result === "string" ? result : result && result.name);
   const suggestionTemplate = result =>
     typeof result === "string" ? result : result && `${result.name} (${result.code})`;
 
-  console.log($input)
   try {
     if($input) {
       accessibleAutocomplete({
@@ -15,7 +50,7 @@ const initAutocomplete = ({element, input, path, selectNameAndCode}) => {
         name: $input.name,
         defaultValue: $input.value,
         minLength: 3,
-        source: request(path),
+        source: debounce(request(path), 900),
         templates: {
           inputValue: selectNameAndCode ? suggestionTemplate : inputValueTemplate,
           suggestion: suggestionTemplate
@@ -27,17 +62,8 @@ const initAutocomplete = ({element, input, path, selectNameAndCode}) => {
       $input.parentNode.removeChild($input);
     }
   } catch(err) {
-    console.error("Failed to initialise provider autocomplete:", err);
+    console.error(`Failed to initialise ${input} autocomplete:`, err);
   }
 };
 
 export default initAutocomplete;
-
-document.addEventListener('DOMContentLoaded', function() {
-
-  initAutocomplete({
-    element: "location-autocomplete",
-    input: "location",
-    path: "/location-suggestions",
-  });
-})
