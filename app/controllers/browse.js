25fca3ea0286e214408cb535c44e6df70b67de90
Simon Whatley <simonwhatley@gmail.com>
2022-09-12 16:43:34 +0100

    Update browse routes

--- app/controllers/browse.js
+++ app/controllers/browse.js
@@ -14,24 +14,36 @@ exports.browse_post = async (req, res) => {
 
 exports.primary_get = async (req, res) => {
 
+  res.render('browse/primary', {
+
+  })
 }
 
 exports.primary_post = async (req, res) => {
 
+  res.redirect('/results')
 }
 
 exports.secondary_get = async (req, res) => {
 
+  res.render('browse/secondary', {
+
+  })
 }
 
 exports.secondary_post = async (req, res) => {
 
+  res.redirect(`/browse/secondary/subjects/${subject}`)
 }
 
 exports.secondary_subject_get = async (req, res) => {
 
+  res.render('browse/subjects', {
+
+  })
 }
 
 exports.secondary_subject_post = async (req, res) => {
 
+  res.redirect('/results')
 }
