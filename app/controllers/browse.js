9c490346ebf66ff687a78a9d2b68443db0c67ffc
Simon Whatley <simonwhatley@gmail.com>
2022-09-13 12:05:00 +0100

    Reorganise browse controller

--- app/controllers/browse.js
+++ app/controllers/browse.js
@@ -1,52 +1,41 @@
-
+const locationSuggestionsService = require('../services/location-suggestions')
 
 exports.browse_get = async (req, res) => {
   if (process.env.USER_JOURNEY === 'search') {
     res.redirect('/search')
   } else {
-    res.render('browse/index')
+    res.render('browse/index', {
+      actions: {
+        primary: '/browse/primary',
+        secondary: '/browse/secondary'
+      }
+    })
   }
 }
 
-exports.browse_post = async (req, res) => {
-
+exports.primary_get = async (req, res) => {
+  req.session.data.ageGroup = 'primary'
+  res.redirect('/browse/location')
 }
 
-exports.primary_get = async (req, res) => {
+exports.secondary_get = async (req, res) => {
+  req.session.data.ageGroup = 'secondary'
+  res.render('browse/secondary-subjects', {
+
+  })
+}
+
+exports.location_get = async (req, res) => {
   const cities = ['Birmingham','Bradford','Bristol','Leeds','Liverpool','London','Manchester','Sheffield']
   const regions = ['North East','North West','Yorkshire and The Humber','East Midlands','West Midlands','East of England','London','South East','South West']
 
-  res.render('browse/primary', {
+  res.render('browse/location', {
+    ageGroup: req.session.data.ageGroup,
     cities,
     regions
   })
 }
 
-exports.primary_post = async (req, res) => {
+exports.location_post = async (req, res) => {
 
-  res.redirect('/results')
-}
-
-exports.secondary_get = async (req, res) => {
-
-  res.render('browse/secondary', {
-
-  })
-}
-
-exports.secondary_post = async (req, res) => {
-
-  res.redirect(`/browse/secondary/subjects/${subject}`)
-}
-
-exports.secondary_subject_get = async (req, res) => {
-
-  res.render('browse/subjects', {
-
-  })
-}
-
-exports.secondary_subject_post = async (req, res) => {
-
-  res.redirect('/results')
 }
