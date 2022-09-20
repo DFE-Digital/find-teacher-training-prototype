521e8185487888d6586805f34caeb1dcb2277d7a
Simon Whatley <simonwhatley@gmail.com>
2022-09-20 15:41:40 +0100

    Add utility helper

---
+++ app/helpers/utils.js
@@ -0,0 +1,17 @@
+exports.getCheckboxValues = (name, data) => {
+  return name && (Array.isArray(name)
+    ? name
+    : [name].filter((name) => {
+        return name !== '_unchecked'
+      })) || data && (Array.isArray(data) ? data : [data])
+}
+
+exports.removeFilter = (value, data) => {
+  // do this check because if coming from overview page for example,
+  // the query/param will be a string value, not an array containing a string
+  if (Array.isArray(data)) {
+    return data.filter(item => item !== value)
+  } else {
+    return null
+  }
+}
