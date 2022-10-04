81f13c0b6fdc87f9cbd944c4da1fb5ca1bcd9062
Simon Whatley <simonwhatley@gmail.com>
2022-10-04 20:07:12 +0100

    Change recruitment cycle to 2023

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,6 +1,6 @@
 module.exports = {
   apiEndpoint: 'https://api.publish-teacher-training-courses.service.gov.uk/api/public/v1',
-  cycle: 2022,
+  cycle: 2023,
   nearingEndOfCycle: false,
   cycleClosed: false,
   defaults: {
