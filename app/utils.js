64fade9aa0a4f2ca12759c872788e5f81ba98ecc
Simon Whatley <simonwhatley@gmail.com>
2022-11-30 20:33:12 +0000

    Use course data to decorate provider

--- app/utils.js
+++ app/utils.js
@@ -113,7 +113,7 @@ module.exports = () => {
     return course
   }
 
-  utils.decorateProvider = provider => {
+  utils.decorateProvider = (provider, courses = []) => {
     // is_accredited_body
     provider.is_accredited_body = !!(provider.provider_type === 'university' || provider.provider_type === 'scitt')
 
@@ -122,6 +122,69 @@ module.exports = () => {
     provider.can_sponsor_both_visas = !!(provider.can_sponsor_student_visa && provider.can_sponsor_skilled_worker_visa)
     provider.can_sponsor_student_visas_only = provider.can_sponsor_student_visa && !provider.can_sponsor_skilled_worker_visa
     provider.can_sponsor_skilled_worker_visas_only = !provider.can_sponsor_student_visa && provider.can_sponsor_skilled_worker_visa
+// console.log(courses);
+
+    provider.has_primary_courses = false
+    provider.primary_courses_count = 0
+    provider.has_secondary_courses = false
+    provider.secondary_courses_count = 0
+    provider.has_further_education_courses = false
+    provider.further_education_courses_count = 0
+
+    provider.has_send_courses = false
+    provider.send_courses_count = 0
+
+    // TODO: reconcile this down to just 'partners'?
+    provider.accredited_bodies = []
+    provider.training_partners = []
+
+    if (courses.length > 0) {
+      courses.map(courseResource => {
+        if (courseResource.course.level === 'primary') {
+          provider.has_primary_courses = true
+          provider.primary_courses_count += 1
+        } else if (courseResource.course.level === 'secondary') {
+          provider.has_secondary_courses = true
+          provider.secondary_courses_count += 1
+        } else if (courseResource.course.level === 'further_education') {
+          provider.has_further_education_courses = true
+          provider.further_education_courses_count += 1
+        }
+
+        if (courseResource.course.is_send) {
+          provider.has_send_courses = true
+          provider.send_courses_count += 1
+        }
+
+        if (provider.is_accredited_body) {
+          // const trainingPartner = {}
+          // trainingPartner.code = ''
+          // trainingPartner.name = ''
+          // if (!provider.training_partners.find(tp => tp.code === trainingPartner.code)) {
+          //   provider.training_partners.push(trainingPartner)
+          // }
+        } else {
+          const accreditedBody = {}
+          accreditedBody.code = courseResource.course.accredited_body_code
+          accreditedBody.name = courseResource.course.accredited_body
+          if (!provider.accredited_bodies.find(ab => ab.code === accreditedBody.code)) {
+            provider.accredited_bodies.push(accreditedBody)
+          }
+        }
+
+      })
+    }
+
+    provider.offers_subject_levels = []
+    if (provider.has_primary_courses) {
+      provider.offers_subject_levels.push('primary')
+    }
+    if (provider.has_secondary_courses) {
+      provider.offers_subject_levels.push('secondary')
+    }
+    if (provider.has_further_education_courses) {
+      provider.offers_subject_levels.push('further education')
+    }
 
     return provider
   }
