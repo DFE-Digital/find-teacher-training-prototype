1cd438c69600d49bd61b6b226b29289c69b00375
Kellie <kelliematheson@gmail.com>
2024-05-29 11:20:06 +0100

    Add school placements page

--- app/controllers/courses.js
+++ app/controllers/courses.js
@@ -158,6 +158,57 @@ exports.showProvider = async (req, res) => {
   }
 }
 
+exports.showSchoolPlacements = async (req, res) => {
+  const providerCode = req.params.providerCode.toUpperCase()
+  const courseCode = req.params.courseCode.toUpperCase()
+
+  try {
+    const courseSingleResponse = await teacherTrainingService.getCourse(providerCode, courseCode)
+    const course = utils.decorateCourse(courseSingleResponse.data.attributes)
+
+    if (course.accredited_body_code) {
+      const providerSingleResponse = await teacherTrainingService.getProvider(course.accredited_body_code)
+      course.accredited_body = utils.decorateProvider(providerSingleResponse)
+    }
+
+    const locationListResponse = await teacherTrainingService.getCourseLocations(providerCode, courseCode)
+    const statuses = locationListResponse.included.filter(item => item.type === 'location_statuses')
+    const locations = locationListResponse.data.map(location => {
+      const { attributes } = location
+
+      // Vacancy status
+      const statusId = location.relationships.location_status.data.id
+      const status = statuses.find(status => status.id === statusId)
+      attributes.has_vacancies = status.attributes.has_vacancies
+
+      // Address
+      const streetAddress1 = attributes.street_address_1 ? attributes.street_address_1 + ', ' : ''
+      const streetAddress2 = attributes.street_address_2 ? attributes.street_address_2 + ', ' : ''
+      const city = attributes.city ? attributes.city + ', ' : ''
+      const county = attributes.county ? attributes.county + ', ' : ''
+      const postcode = attributes.postcode
+
+      //attributes.name = attributes.name.replace(/'/g, ''')
+      //attributes.address = `${streetAddress1}${streetAddress2}${city}${county}${postcode}`
+
+      return attributes
+    })
+
+    res.render('course/school-placements-2024', {
+      course,
+      actions: {
+        back: `/providers/${req.params.providerCode}/courses/${req.params.courseCode}`
+      }
+    })
+
+  } catch (error) {
+    res.render('error', {
+      title: error.name,
+      content: error.message
+    })
+  }
+}
+
 exports.show = async (req, res) => {
   const providerCode = req.params.providerCode.toUpperCase()
   const courseCode = req.params.courseCode.toUpperCase()
